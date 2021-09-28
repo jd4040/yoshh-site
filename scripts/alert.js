cecaf3810cd58fdd990f059470ae6b0b59042425
Johnson Duong <yoshh4040@gmail.com>
2021-09-28 03:50:36 -0700

    Added script to Fun page. Script consists of alert, prompt, confirm functions

---
+++ scripts/alert.js
@@ -0,0 +1,44 @@
+"use strict";
+//Alert! :)
+
+/* Alert
+the
+user! */
+
+/* Commenting out the code
+alert("Don't show this!");
+
+Tip: "Ctrl + /" Comments out a code line!
+*/
+
+let message;
+
+message="Hello Another World!"; //Store the string into variable named message
+
+let hello_again_message = " Hello.. again.."; //Use camelCase!
+let helloAgainMessage = hello_again_message; // Better ! :)
+
+let message01 = "Welcome ", message02 = "to my ", message03 = "site!";
+
+const MY_SITE_NAME = "Yoshh.moe";
+const BIG_INT = 249058349058902348490234234982349082390482390489023849023890433845n;
+
+let isGreater = 5>1;
+
+// alert(message + hello_again_message);
+// alert(helloAgainMessage);
+// alert(message01+message02+message03 + " " + MY_SITE_NAME);
+// alert("Look at this big number: " + BIG_INT);
+
+// ${} within backticks (`Hello`) allows you to evaluate expressions within a string
+
+
+// Create modal window with the message. A modal window is a window that must be dealt with before you can interact with the site
+alert(`Hello, welcome to ${MY_SITE_NAME}!`)
+
+/* Asks the user to rate the site from 1-10, but takes in any value and returns it as a string */
+let resultFirstPrompt = prompt(`Rate my site, ${MY_SITE_NAME}, from 1-10`, '');
+alert(`Wow, you gave it a ${resultFirstPrompt}!`);
+
+/* Asks the user for confirmation to enter the site, but the site loads regardless of actual confirmation */
+let enterConfirmation = confirm(`Are you sure you want to enter ${MY_SITE_NAME}?`);
